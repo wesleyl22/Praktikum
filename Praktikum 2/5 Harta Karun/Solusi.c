ece2e9d174a0579c83bcf9c132868000837ad525
Wesley L <wesleylianto@gmail.com>
2026-03-24 11:52:29 +0700

    fix: fix prak 2 no 5 to not use gradient (NaN/inf errors)

--- Praktikum 2/5 Harta Karun/Solusi.c
+++ Praktikum 2/5 Harta Karun/Solusi.c
@@ -2,82 +2,57 @@
 #include <math.h>
 #include <float.h>
 
-double cekParalel(double m1, double m2) {
-    return m1 == m2;
-}
-
-double getGradien(double x1, double y1, double x2, double y2) {
-    return (y2-y1)/(x2-x1);
-}
-
-double getTipotX(double m1, double x1, double y1, double m2, double x2, double y2) {
-    return ((m1*x1 - m2*x2 + y2 - y1) / (m1 - m2));
-}
-
-double getY(double m1, double x1, double y1, double x) {
-    return (m1*(x-x1) + y1);
-}
-
 double getJarak(double x1, double y1, double x2, double y2) {
-    return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
 
-double titikPotong(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double *tipotX, double *tipotY) {
-    double m1 = getGradien(x1, y1, x2, y2);
-    double m2 = getGradien(x3, y3, x4, y4);
-
-    double X = getTipotX(m1, x1, y1, m2, x3, y3);
-    *tipotX = X;
-    *tipotY = getY(m1, x1, y1, X);
+int titikPotong(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double *tX, double *tY) {
+    
+    double den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+    
+    if (den == 0) {
+        return 0; 
+    }
+    
+    *tX = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / den;
+    *tY = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / den;
+    
+    return 1;
 }
 
 int main() {
+    double x_A, y_A, x_B, y_B, x_C, y_C, x_D, y_D, x_E, y_E;
 
-    double x_A, x_B, x_C, x_D, x_E;
-    double y_A, y_B, y_C, y_D, y_E;
+    scanf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", 
+          &x_A, &y_A, &x_B, &y_B, &x_C, &y_C, &x_D, &y_D, &x_E, &y_E);
 
-    scanf("%lf %lf %lf %lf %lf %lf %lf %lf %lf %lf", &x_A, &y_A, &x_B, &y_B, &x_C, &y_C, &x_D, &y_D, &x_E, &y_E);
+    double n1 = -1, n2 = -1, n3 = -1;
+    double tX, tY;
 
-    double n1 = -1;
-    double n2 = -1;
-    double n3 = -1;
-
-    // AB CD
-
-    if (cekParalel(getGradien(x_A, y_A, x_B, y_B), getGradien(x_C, y_C, x_D, y_D)) == 0) {
-    double x_ABCD, y_ABCD;
-    
-    titikPotong(x_A, y_A, x_B, y_B, x_C, y_C, x_D, y_D, &x_ABCD, &y_ABCD);
-    n1 = getJarak(x_ABCD, x_ABCD, x_E, y_E);
+    // AB dan CD
+    if (titikPotong(x_A, y_A, x_B, y_B, x_C, y_C, x_D, y_D, &tX, &tY)) {
+        n1 = getJarak(tX, tY, x_E, y_E);
     }
 
-    // AC BD
-
-    if (cekParalel(getGradien(x_A, y_A, x_C, y_C), getGradien(x_B, y_B, x_D, y_D)) == 0) {
-    double x_ACBD, y_ACBD;
-
-    titikPotong(x_A, y_A, x_C, y_C, x_B, y_B, x_D, y_D, &x_ACBD, &y_ACBD);
-    n2 = getJarak(x_ACBD, y_ACBD, x_E, y_E);
+    // AC dan BD
+    if (titikPotong(x_A, y_A, x_C, y_C, x_B, y_B, x_D, y_D, &tX, &tY)) {
+        n2 = getJarak(tX, tY, x_E, y_E);
     }
 
-    // AD BC
-
-    if (cekParalel(getGradien(x_A, y_A, x_D, y_D), getGradien(x_B, y_B, x_C, y_C)) == 0) {
-    double x_ADBC, y_ADBC;
-    
-    titikPotong(x_A, y_A, x_D, y_D, x_B, y_B, x_C, y_C, &x_ADBC, &y_ADBC);
-    n3 = getJarak(x_ADBC, y_ADBC, x_E, y_E);
+    // AD dan BC
+    if (titikPotong(x_A, y_A, x_D, y_D, x_B, y_B, x_C, y_C, &tX, &tY)) {
+        n3 = getJarak(tX, tY, x_E, y_E);
     }
 
-    double lowest = 0;
+    double lowest = DBL_MAX;
 
-    if (n1 >= lowest) {
+    if (n1 >= 0 && n1 < lowest) {
         lowest = n1;
     }
-    if (n2 >= lowest) {
+    if (n2 >= 0 && n2 < lowest) {
         lowest = n2;
     }
-    if (n3 >= lowest) {
+    if (n3 >= 0 && n3 < lowest) {
         lowest = n3;
     }
 
